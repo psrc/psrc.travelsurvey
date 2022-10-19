ae2de92c290894e65cf523d6815156af3023e3e9
Suzanne Childress <childresssuzanne@gmail.com>
2022-10-19 11:54:49 -0700

    accidentally switched the select statements for sqlite and sqlserver

--- R/get_hhts.R
+++ R/get_hhts.R
@@ -149,8 +149,7 @@ get_hhts <- function(survey, level, vars, ...){
                         "HHSurvey.v_days",
                         "HHSurvey.v_vehicles"),2)) %>% setDT()
        sql_tbl_ref <- sql_hhts_lookup[abbr==level, .(tbl_ref)][[1]]# Convert level to view name       
-       sql_code<-paste("SELECT * FROM",sql_tbl_ref,"LIMIT 1;")
-     
+       sql_code <- paste("SELECT TOP 1 * FROM",sql_tbl_ref,";")
      }
     else{
       
@@ -161,8 +160,8 @@ get_hhts <- function(survey, level, vars, ...){
                       "[HHSurvey.v_trips]",
                       "[HHSurvey.v_days]",
                       "[HHSurvey.v_vehicles]"),2)) %>% setDT()
-     sql_tbl_ref <- sql_hhts_lookup[abbr==level, .(tbl_ref)][[1]]# Convert level to view name       
-     sql_code <- paste("SELECT TOP 1 * FROM",sql_tbl_ref,";")
+     sql_tbl_ref <- sql_hhts_lookup[abbr==level, .(tbl_ref)][[1]]# Convert level to view name
+     sql_code<-paste("SELECT * FROM",sql_tbl_ref,"LIMIT 1;")
     }
     
     

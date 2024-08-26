791301aab505928ad946b4c40f0b5ae721f1bbeb
jensenmj <mjensen@psrc.org>
2024-08-26 15:21:33 -0700

    Fix parenthesis error.

--- R/psrc-hts-recode-trip-vars.R
+++ R/psrc-hts-recode-trip-vars.R
@@ -26,26 +26,26 @@ hts_bin_dest_purpose <- function(hts_data){
   }else{
     hts_data$trip %<>% setDT() %>% 
       .[, dest_purpose_bin11:=factor(
-        fcase(grepl("^Went (home|to another(residence|temporary))"), as.character(dest_purpose),       "Home",
-              grepl("^(Attend|Went) .* (college|school|education|class)"), as.character(dest_purpose), "School",
+        fcase(grepl("^Went (home|to another(residence|temporary))", as.character(dest_purpose)),       "Home",
+              grepl("^(Attend|Went) .* (college|school|education|class)", as.character(dest_purpose)), "School",
               as.character(dest_purpose)=="Went to primary workplace",                                 "Primary work",
-              grepl("Went to( other)? work-related"), as.character(dest_purpose),                      "Work-related",
-              grepl("([pP]ick(ed)? up)|([dD]rop(ped)? off)"), as.character(dest_purpose),              "Pick up/Drop off",
-              grepl("(shopping|^Got gas)"), as.character(dest_purpose),                                "Shopping",              
+              grepl("Went to( other)? work-related", as.character(dest_purpose)),                      "Work-related",
+              grepl("([pP]ick(ed)? up)|([dD]rop(ped)? off)", as.character(dest_purpose)),              "Pick up/Drop off",
+              grepl("(shopping|^Got gas)", as.character(dest_purpose)),                                "Shopping",              
               grepl("\\beat\\b", as.character(dest_purpose)),                                          "Eat Meal",
               grepl("([sS]ocial|[rR]ecreation|exercise|[vV]olunteer|Vacation|family activity)", 
                     as.character(dest_purpose)),                                                       "Social/Recreation",
               !is.na(dest_purpose),                                                                    "Errands/Other"),
         levels=c("Home","Primary work","Work-related","School","Pick up/Drop off",
-                 "Shopping","Social/Recreation", "Errands/Appointments/Other"))]
+                 "Shopping","Social/Recreation", "Errands/Other"))]
     labelled::var_label(hts_data$trip$dest_purpose_bin11) <- "Destination Purpose"
     hts_data$trip %>%
       .[, dest_purpose_bin4:=factor(
         fcase(as.character(dest_purpose_bin11)=="Home",                               "Home",
               as.character(dest_purpose_bin11) %in% c("Primary work","Work-related"), "Work",
-              grepl("^(Eat|Social)"), as.character(dest_purpose_bin11),               "Social/Recreation/Meal",
+              grepl("^(Eat|Social)", as.character(dest_purpose_bin11)),               "Social/Recreation/Meal",
               !is.na(dest_purpose_bin11),                                             "Errands/Other"),
-        levels=c("Home","Work","Social, Recreation, Eat Meal","Errands/Appointments/Other"))]
+        levels=c("Home","Work","Social/Recreation/Meal","Errands/Other"))]
     labelled::var_label(hts_data$trip$dest_purpose_bin4) <- "Destination Purpose"
   }
   return(hts_data)

08c9ed919ca823e89cb0551b7d65b723ba35d1e8
jensenmj <mjensen@psrc.org>
2024-12-10 10:38:56 -0800

    Drop extra columns.

--- R/internal_data.R
+++ R/internal_data.R
@@ -12,6 +12,6 @@ NULL
 # filepath <- "J:/Projects/Surveys/HHTravel/Survey2023/Data/data_published/PSRC_Codebook_2023_v1.xlsx"
 # variable_list <- read_xlsx(filepath, sheet="variable_list") %>% setDT() %>% .[, logic:=NULL]
 # variable_list[is.na(shared_name), shared_name:=variable]
-# value_labels <- read_xlsx(filepath, sheet="value_labels") %>% setDT()
+# value_labels <- read_xlsx(filepath, sheet="value_labels") %>% setDT() %>% .[, 1:4]
 
 # usethis::use_data(variable_list, value_labels, internal=TRUE, overwrite=TRUE)           # Makes part of the package; push this to repo
